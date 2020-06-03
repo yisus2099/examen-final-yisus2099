f20f56f87c76aa9815fe5c6dc235b0ef25c18bad
yisus2099 <65199911+yisus2099@users.noreply.github.com>
2020-06-03 16:06:36 -0600

    Edite index.js y cree index.hbs

--- index.js
+++ index.js
@@ -2,5 +2,21 @@
 const express = require('express');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
-
+//Constante del puerto
 const port = process.env.PORT || 3000;
+
+//Emplear rutas
+let pintoresRouter = require('./routes/pintor');
+
+//Sitio web y HBS
+const app = express();
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+app.use(express.static(__dirname+'/public'));
+
+//Ruta 
+app,use('/', pintoresRouter);
+
+//Conexion con MongoDB
+mongoose.Promise = global.Promise;
+const cadena = 'nuestra cadena de conexion de mongo'
