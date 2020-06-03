ee54f1b4a2720adb2b61f9b54c74cee1f5a0d301
yisus2099 <65199911+yisus2099@users.noreply.github.com>
2020-06-03 15:31:06 -0600

    Cree el modelo

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 const express = require('express');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
+
+const port = process.env.PORT || 3000;
---
+++ models/pintores.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+//Schema
+
+let Schema = mongoose.Schema;
+
+//Colección
+let PintoresSchema = new Schema({
+    name:{
+        type: String,
+        required: [true, 'Requerimos el nombre']
+    },
+    corriente:{
+        type: String,
+        required:[true, 'Requerimos la corriente']
+    },
+    nacionalidad:{
+        type: String,
+        required:[true, 'Requerimos la corriente']
+    },
+    pintura:{
+        type: String,
+        required:[true, 'Requerimos la corriente']
+    }
+    
+});
