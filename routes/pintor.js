950ab704ebabd7e343e20e42374bb60898bd9e99
yisus2099 <65199911+yisus2099@users.noreply.github.com>
2020-06-03 15:58:16 -0600

    Cree rutas

---
+++ routes/pintor.js
@@ -0,0 +1,10 @@
+const express = require('express');
+//Routear 
+let pintor = require('../controllers/Pintorescontroller.js');
+
+//rutas
+let router = express.Router();
+
+router.get('/', pintor.list());
+
+module.exports = router;
